bcba511feac3fb15179e53d51ae239db14ba1a26
atnguy43 <99783158+atnguy43@users.noreply.github.com>
2025-09-27 01:15:39 -0700

    Added layout folder for pages
    Makes layout consistent along all other pages

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+const _layout = () => {
+    return (
+        <View>
+            <Text>_layout</Text>
+        </View>
+    )
+}
+
+export default _layout
+const styles = StyleSheet.create({})
