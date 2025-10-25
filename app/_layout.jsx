8bbaa8205acf67c3f8f74b4b7d984a0f0fc10e51
atnguy43 <99783158+atnguy43@users.noreply.github.com>
2025-10-24 20:21:41 -0700

    Update _layout.jsx
    Added automatic light/dark mode detection and color schemes
    Allows for consistent coloring

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, useColorScheme, Text, View } from 'react-native'
 import { Slot, Stack } from 'expo-router'
+import { Colors } from "../constants/Colors"
 
 
-const RootLayout = () => {
+const RootLayout = () => { 
+    const colorScheme = useColorScheme() 
+    console.log(colorScheme)
+    const theme = Colors[colorScheme] ?? Colors.light
+
     return (
-        <Stack>
+        <Stack screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+        }}>
             <Stack.Screen name="index" options={{ title: 'Home' }} />
             <Stack.Screen name="startShower" options={{ title: 'Routines' }} />
             <Stack.Screen name="routines" options={{ title: 'View Routines' }} />
             <Stack.Screen name="statistics" options={{ title: 'View Stats' }} />
-            <Stack.Screen name="appSettings" options={{ title: 'Settings' }} />
+            <Stack.Screen name="appSettings" options={{ title: 'Settings', headerShown: false }} />
         </Stack>
     )
 }
