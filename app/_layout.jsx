17b7c49f1fb16d888bc012878bae2eda0133af6a
atnguy43 <99783158+atnguy43@users.noreply.github.com>
2025-10-24 19:26:04 -0700

    Update _layout.jsx
    Added Titles to each page and back button to pop page from stack

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
-const _layout = () => {
+import { Slot, Stack } from 'expo-router'
+
+
+const RootLayout = () => {
     return (
-        <View>
-            <Text>_layout</Text>
-        </View>
+        <Stack>
+            <Stack.Screen name="index" options={{ title: 'Home' }} />
+            <Stack.Screen name="startShower" options={{ title: 'Routines' }} />
+            <Stack.Screen name="routines" options={{ title: 'View Routines' }} />
+            <Stack.Screen name="statistics" options={{ title: 'View Stats' }} />
+            <Stack.Screen name="appSettings" options={{ title: 'Settings' }} />
+        </Stack>
     )
 }
 
-export default _layout
+export default RootLayout
+
 const styles = StyleSheet.create({})
