e0d7fca8992483247043d15f3cbd7706513f07f4
atnguy43 <99783158+atnguy43@users.noreply.github.com>
2025-11-26 18:25:41 -0700

    Update _layout.jsx
    Added timer button

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, useColorScheme, Text, View } from 'react-native'
 import { Slot, Stack } from 'expo-router'
 import { Colors } from "../constants/Colors"
+import { StatusBar } from 'expo-status-bar' 
 
 
 const RootLayout = () => { 
@@ -10,18 +11,23 @@ const RootLayout = () => {
 
     // defaults to light theme if colorScheme is null
     const theme = Colors[colorScheme] ?? Colors.light
-
+    
+    
     return (
-        <Stack screenOptions={{
-            headerStyle: { backgroundColor: theme.navBackground },
-            headerTintColor: theme.title,
-        }}>
-            <Stack.Screen name="index" options={{ title: 'Home' }} />
-            <Stack.Screen name="startShower" options={{ title: 'Routines' }} />
-            <Stack.Screen name="routines" options={{ title: 'View Routines' }} />
-            <Stack.Screen name="statistics" options={{ title: 'View Stats' }} />
-            <Stack.Screen name="appSettings" options={{ title: 'Settings', headerShown: false }} />
-        </Stack>
+        <>
+            <StatusBar value="auto" />
+            <Stack screenOptions={{
+                headerStyle: { backgroundColor: theme.navBackground },
+                headerTintColor: theme.title,
+            }}>
+                <Stack.Screen name="index" options={{ title: 'Home' }} />
+                <Stack.Screen name="startShower" options={{ title: 'Routines' }} />
+                <Stack.Screen name="timer" options={{ title: 'Timer' }} />
+                <Stack.Screen name="routines" options={{ title: 'View Routines' }} />
+                <Stack.Screen name="statistics" options={{ title: 'View Stats' }} />
+                <Stack.Screen name="appSettings" options={{ title: 'Settings', headerShown: false }} />
+            </Stack>
+        </>
     )
 }
 
