16d05bfa60cbc869ed162ed1d86ffe9bf6ea14e7
atnguy43 <99783158+atnguy43@users.noreply.github.com>
2025-11-26 17:45:53 -0700

    Create ThemedCard.jsx

---
+++ components/ThemedCard.jsx
@@ -0,0 +1,22 @@
+import { View, useColorScheme } from 'react-native'
+import { Colors } from '../constants/Colors'
+
+const ThemedCard = ({ style, ...props }) => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+
+    return (
+        <View 
+            style={[{ backgroundColor: theme.uiBackground}, styles.card, style]}
+            {...props}
+        />
+    )
+}
+
+export default ThemedCard
+const styles = SystelSheet.create({
+    card: {
+        borderRadius: 5,
+        padding: 20
+    }
+})
